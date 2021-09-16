24d0928092735d80f876ec5174c4097010ee91cb
NNQA <81368386+NNQA@users.noreply.github.com>
2021-09-16 14:22:22 +0700

    Update climbingtheLeaderboard.cpp

--- algorithms/climbingtheLeaderboard.cpp
+++ algorithms/climbingtheLeaderboard.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+#include <stack>
+
+int main() {
+    unsigned long n, m ,tmp;
+    std::cin >> n;
+    
+    std::stack<unsigned long> sroces;
+    
+    for(int i = 0 ; i != n; i++) {
+        std::cin >> tmp;
+        if(sroces.empty() || tmp != sroces.top()) {
+            sroces.push(tmp);
+        }
+    }
+    
+    std::cin >> m;
+    for(int i = 0 ; i != m; i++) {
+        std::cin >> tmp;
+        while(!sroces.empty() && tmp >= sroces.top()) {
+            sroces.pop();
+        }
+        std::cout << (sroces.size() + 1) << std::endl;
+    }
+    
+}
